cb9f7c83515e7b84f2bf42e01c992327aab5969a
Sascha Kondring <zer088@web.de>
2018-11-02 15:00:29 +0100

    search for movietitles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { MovieInformationComponent } from './movie-information/movie-information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    MovieInformationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movie.ts
+++ src/app/movie.ts
@@ -1,3 +1,39 @@
+import { Actor } from './actor';
+
 export class Movie {
     title: string;
+    actors: Array<Actor>;
+    plot: string;
+    genre: Array<string>;
+    rating: number;
+    year: number;
+    poster: string;
+    
+    constructor(private t:string, private a:string, private pl:string, private g:string, private r:string, private y:string, private p:string) {
+        this.title = t;
+        a.split(',').forEach(fullName => {
+            let fullNameArray:Array<string> = fullName.trim().split(' ');
+            let actor:Actor = new Actor(fullNameArray[0], fullNameArray[1]);
+            if (typeof this.actors == 'undefined') this.actors = new Array<Actor>();
+            this.actors.push(actor);
+        });
+
+        g.split(',').forEach(gen => {
+            if (typeof this.genre == "undefined") this.genre = new Array<string>();
+            this.genre.push(gen.trim());
+        });
+
+        this.rating = Number(r);
+        this.year = Number(y);
+        this.poster = p;
+        this.plot = pl;
+
+        delete this.t;
+        delete this.a;
+        delete this.pl;
+        delete this.g;
+        delete this.r;
+        delete this.y;
+        delete this.p;
+    }
 }
